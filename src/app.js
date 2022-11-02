e461e0c829da928b793f39cc55682d15c8d92656
ThinhNguyen <thinhtheblues1102@gmail.com>
2022-11-02 22:46:06 +0700

    add: dummy data test

--- src/app.js
+++ src/app.js
@@ -12,6 +12,34 @@ app.use(cors());
 
 app.use("/api/get-text/:url", require("./helpers/handleFetchText"));
 
+app.use("/data", (req, res, next) => {
+  const data = [
+    {
+      name: "John",
+      age: 30,
+      cars: [
+        { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+        { name: "BMW", models: ["320", "X3"] },
+      ],
+    },
+    {
+      name: "Maria",
+      age: 25,
+      cars: [{ name: "Fiat", models: ["500", "Panda"] }],
+    },
+    {
+      name: "David",
+      age: 40,
+      cars: [
+        { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+        { name: "BMW", models: ["320", "X3", "X5"] },
+        { name: "Fiat", models: ["500", "Panda"] },
+      ],
+    },
+  ];
+  res.status(200).json(data);
+});
+
 app.use("*", (req, res, next) => {
   res.send(`<h4>nguyen van thinh</h4>
   <h5>Link text 123doc</h5>
